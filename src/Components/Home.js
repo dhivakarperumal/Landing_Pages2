51877d4900c8ac77d3170c77fc74181dccfcc688
dhivakarperumal <142518349+dhivakarperumal@users.noreply.github.com>
2023-09-22 17:48:17 +0530

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Banners from "../Components12/Banner";
+
+
+function Home() {
+    return (
+
+        <>
+
+            <Banners/>
+
+        </>
+
+
+
+    )
+}
+export default Home
